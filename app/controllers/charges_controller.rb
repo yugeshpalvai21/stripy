a097f8d12e159ca209d53117e2491546968a12d2
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-04-17 23:59:29 +0530

    upgrades stripe from legacy checkout to new checkout

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -3,6 +3,23 @@ class ChargesController < ApplicationController
   
   def new
     @product = Product.find(params[:product])
+    @session = Stripe::Checkout::Session.create({
+      payment_method_types: ['card'],
+      line_items: [{
+        price_data: {
+          currency: 'inr',
+          product_data: {
+            name: 'New Product'
+          },
+          unit_amount: 2000,
+        },
+        quantity: 1,
+      }],
+      mode: 'payment',
+      # These placeholder URLs will be replaced in a following step.
+      success_url: 'http://localhost:3000/charges/success',
+      cancel_url: 'http://localhost:3000/charges/cancel',
+    })
   end
 
   def create
